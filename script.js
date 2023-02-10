0a5612a2965a9e1599c0d5c9f8904ca93e31136f
Andrew Breytenbach <117181767+andrewbreytenbach@users.noreply.github.com>
2023-02-10 18:45:53 -0500

    made some minor changes to js file

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 
 // Add event listener to generate button. I didn't need the var generateBtn = document.querySelector("#generate"); that came with the starter code if I wrote it this way. 
 var generatedPassword = document.getElementById("generate").addEventListener("click", function() {
-  var passwordPrompt = prompt("How many characters would you like your password to have? Please enter a number between 8 and 32 characters.");
+  var passwordPrompt = prompt("How many characters would you like your password to have? Please enter a number between 8 and 128 characters.");
+  
+  if (passwordPrompt === null) {
+    return;
+  }
+// This creates an arrg 
+var passwordCharacters = [];
+
+var upperCaseOption = confirm("Include uppercase characters in your password?");
+if (upperCaseOption) {
+  passwordCharacters = passwordCharacters.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
+}
+
 })
