b8d397ec1c110f743b84ca19d0246073359abbf0
Andrew Breytenbach <ambreyts15@gmail.com>
2023-02-10 19:25:13 -0500

    wrote in a for loop to randomize the password

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 
+// Add event listener to generate button. I didn't need the var generateBtn = document.querySelector("#generate"); that came with the starter code if I wrote it this way. 
+var generatedPassword = document.getElementById("generate").addEventListener("click", function() {
+  var passwordPrompt = prompt("How many characters would you like your password to have? Please enter a number between 8 and 128 characters.");
+  
+  if (passwordPrompt === null) {
+    return;
+  }}
 // Assignment code here
+, function generatePassword() {
 
+  for (var i = 0; i <= passwordLength; i++) {
+  var randomUpper = Math.floor(Math.random() * chars.length);
+  password += chars.substring(randomNumber, randomNumber +1);
+ }})
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -16,5 +28,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
