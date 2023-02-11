fd6cf67d54c6650f45e90f9801cb3cf9864dffa7
Andrew Breytenbach <ambreyts15@gmail.com>
2023-02-10 19:17:38 -0500

    started over from original starter code. I got myself really mixed up

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 
-// Add event listener to generate button. I didn't need the var generateBtn = document.querySelector("#generate"); that came with the starter code if I wrote it this way. 
-var generatedPassword = document.getElementById("generate").addEventListener("click", function() {
-  var passwordPrompt = prompt("How many characters would you like your password to have? Please enter a number between 8 and 128 characters.");
-  
-  if (passwordPrompt === null) {
-    return;
-  }
-// This creates an arrg 
-var passwordCharacters = [];
+// Assignment code here
 
-var upperCaseOption = confirm("Include uppercase characters in your password?");
-if (upperCaseOption) {
-  passwordCharacters = passwordCharacters.concat("ABCDEFGHIJKLMNOPQRSTUVWXYZ".split(""));
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
 
-})
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
